2fab1d2f4638a9f8407377f98de54ad45877e15c
Treinamento Asus <pablo.santos@ccc.ufcg.edu.br>
2017-12-13 17:14:37 -0300

    Created initial view

--- app/src/main/java/com/embedded/treinamento/mytrip/MainActivity.kt
+++ app/src/main/java/com/embedded/treinamento/mytrip/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.embedded.treinamento.mytrip
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
